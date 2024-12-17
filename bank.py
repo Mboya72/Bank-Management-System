61acbd65b47e76ee058deca97a70d38d2d88b0c7
Mboya72 <elvindio72@gmail.com>
2024-12-17 19:22:34 +0300

    creating a cursor object and the accounts table

--- bank.py
+++ bank.py
@@ -2,3 +2,14 @@ import sqlite3
 import decimal
 
 db = sqlite3.connect('bank.db')
+
+cursor = db.cursor()
+
+cursor.execute("""
+    CREATE TABLE IF NOT EXISTS accounts (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT NOT NULL,
+        balance DECIMAL NOT NULL
+    )
+""")
+db.commit()
